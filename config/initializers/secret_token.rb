57e51267beab5f48f27ce995888996d83dd02d39
pivotal <pivotal@noble.nyc.pivotallabs.com>
2013-04-03 15:08:35 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuditLicenseAudit::Application.config.secret_token = 'ae540d92c4d1a232da26d39ba4ff4aa352d5645c2244ebd83749630fef8024a753366777c109a2a7551b385158a95e75500735c4356fd4a5fdccc5cbc16a782a'
